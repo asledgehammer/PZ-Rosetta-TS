55aa59e183e6897cc7291828f3ebf37f050e3993
JabDoesThings <JabDoesThings@github.com>
2023-06-17 10:42:08 -0500

    Fixed parameters and returns not parsing in LuaFunction.

--- src/asledgehammer/rosetta/lua/RosettaLuaFunction.ts
+++ src/asledgehammer/rosetta/lua/RosettaLuaFunction.ts
@@ -6,10 +6,9 @@ import { RosettaLuaParameter } from './RosettaLuaParameter';
 import { RosettaLuaReturns } from './RosettaLuaReturns';
 
 export class RosettaLuaFunction extends RosettaEntity {
-  readonly parameters: RosettaLuaParameter[] = [];
-  returns: RosettaLuaReturns | undefined;
-
   readonly name: string;
+  parameters: RosettaLuaParameter[] = [];
+  returns: RosettaLuaReturns;
   notes: string | undefined;
   deprecated: boolean | undefined;
 
@@ -22,11 +21,6 @@ export class RosettaLuaFunction extends RosettaEntity {
 
     /* PROPERTIES */
     this.notes = this.readNotes();
-  }
-
-  parse(raw: { [key: string]: any }) {
-    /* PROPERTIES */
-    this.notes = this.readNotes(raw);
 
     /* PARAMETERS */
     if (raw.parameters !== undefined) {
@@ -42,5 +36,35 @@ export class RosettaLuaFunction extends RosettaEntity {
       throw new Error(`Method does not have returns definition: ${this.name}`);
     }
     this.returns = new RosettaLuaReturns(raw.returns);
+  }
+
+  parse(raw: { [key: string]: any }) {
+    /* PROPERTIES */
+    this.notes = this.readNotes(raw);
+
+    /* PARAMETERS */
+    if (raw.parameters !== undefined) {
+      const rawParameters: { [key: string]: any }[] = raw.parameters;
+
+      /*
+       * (To prevent deep-logic issues, check to see if Rosetta's parameters match the length of
+       *  the overriding parameters. If not, this is the fault of the patch, not Rosetta)
+       */
+      if (this.parameters.length !== rawParameters.length) {
+        throw new Error(
+          `The lua function ${this.name}'s parameters does not match the parameters to override. (method: ${this.parameters.length}, given: ${rawParameters.length})`,
+        );
+      }
+
+      for (let index = 0; index < this.parameters.length; index++) {
+        this.parameters[index].parse(rawParameters[index]);
+      }
+    }
+
+    /* RETURNS */
+    if (raw.returns === undefined) {
+      throw new Error(`Lua function does not have returns definition: ${this.name}`);
+    }
+    this.returns.parse(raw.returns);
   }
 }
